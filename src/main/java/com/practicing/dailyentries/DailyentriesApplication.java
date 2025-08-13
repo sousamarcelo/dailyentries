e89e20b900bdfcf366b4d72458e7e761b043539d
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-13 20:38:41 -0300

    Início do desenvolvimento de uma API em Spring Boot para gerenciamento de gastos pessoais

---
+++ src/main/java/com/practicing/dailyentries/DailyentriesApplication.java
@@ -0,0 +1,13 @@
+package com.practicing.dailyentries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyentriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyentriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicing/dailyentries/DailyentriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicing.dailyentries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyentriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

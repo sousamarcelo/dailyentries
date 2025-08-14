41c824e1a70da76a5a0c535a863aca89757dd0a6
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-13 23:41:51 -0300

    Atualizado seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+--CADASTRO CATEGORIAS:
 INSERT INTO tb_category(name) VALUES ('Casa');
 INSERT INTO tb_category(name) VALUES ('Estética');
 INSERT INTO tb_category(name) VALUES ('Saude');
@@ -5,3 +6,23 @@ INSERT INTO tb_category(name) VALUES ('Pet');
 INSERT INTO tb_category(name) VALUES ('Lazer');
 INSERT INTO tb_category(name) VALUES ('Transporte');
 INSERT INTO tb_category(name) VALUES ('Viagem');
+
+--CADASTRO USUARIOS:
+INSERT INTO tb_user (name, email, phone, password) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '1234567');
+INSERT INTO tb_user (name, email, phone, password) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '1234567');
+
+--CADASTRO DESPESAS DIARIAS
+
+-- CADASTRO DESPESAS USUARIO 1: Maria Brown
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Compra mensal', 'Atacadão', 1000.00, 2, '2025-08-08', 1, 1);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Manutenção jardim', 'Compra de plantas e adubo', 150.50, 1, '2025-08-05', 1, 1);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Corte de cabelo', 'Salão Beleza VIP', 80.00, 2, '2025-08-06', 1, 2);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Consulta médica', 'Clínica Vida Saudável', 250.00, 0, '2025-08-03', 1, 3);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Ração', 'Pet Shop Feliz', 120.00, 2, '2025-08-04', 1, 4);
+
+-- CADASTRO DESPESAS USUARIO 2: Alex Green
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Cinema', 'Ingresso + pipoca', 75.00, 2, '2025-08-07', 2, 5);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Combustível', 'Posto Shell', 200.00, 2, '2025-08-09', 2, 6);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Passagem aérea', 'Latam', 1500.00, 1, '2025-08-10', 2, 7);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Hotel', 'Pousada Sol e Mar', 900.00, 0, '2025-08-11', 2, 7);
+INSERT INTO tb_expenses (name, description, amount, status, date, user_id, category_id) VALUES ('Manutenção carro', 'Troca de óleo e filtros', 350.00, 2, '2025-08-02', 2, 6);

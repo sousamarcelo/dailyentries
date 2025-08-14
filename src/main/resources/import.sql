63400c67f8f097cb9d1d1f8997ff4cdb96d3b167
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-13 21:22:11 -0300

    Banco H2 validado, seeding do banco tabela Category

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category(name) VALUES ('Casa');
+INSERT INTO tb_category(name) VALUES ('Estética');
+INSERT INTO tb_category(name) VALUES ('Saude');
+INSERT INTO tb_category(name) VALUES ('Pet');
+INSERT INTO tb_category(name) VALUES ('Lazer');
+INSERT INTO tb_category(name) VALUES ('Transporte');
+INSERT INTO tb_category(name) VALUES ('Viagem');

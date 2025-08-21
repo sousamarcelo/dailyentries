a389e4792bd4dbe4d470b51da06fa4370e239690
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-21 17:19:18 -0300

    Mapeamento JPA entidade 'Role'

--- src/main/java/com/practicing/dailyentries/entities/Role.java
+++ src/main/java/com/practicing/dailyentries/entities/Role.java
@@ -2,8 +2,19 @@ package com.practicing.dailyentries.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_role")
 public class Role {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
 	
